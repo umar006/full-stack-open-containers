826694020cf65753d7bf26663542d1498a0b598f
umar006 <umaralfaruq597@gmail.com>
2023-12-24 16:38:36 +0700

    Implement get todo by id

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,7 +35,7 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
