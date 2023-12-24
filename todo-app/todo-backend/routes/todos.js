788520c9851e9c117a4b9d7cb308e16538ee0742
umar006 <umaralfaruq597@gmail.com>
2023-12-24 17:38:21 +0700

    Implement update todo by id

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -40,7 +40,9 @@ singleRouter.get('/', async (req, res) => {
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const updatedTodo = await  req.todo.updateOne(req.body, { new: true });
+
+  res.send(updatedTodo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
