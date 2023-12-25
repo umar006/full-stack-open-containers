d3efc971d04237ee939dd94870a75b4c07fb0a1e
umar006 <umaralfaruq597@gmail.com>
2023-12-25 08:21:48 +0700

    Create api to get the number of created todos in redis

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+const redis = require('../redis');
 const configs = require('../util/config')
 
 let visits = 0
@@ -13,6 +14,12 @@ router.get('/', async (req, res) => {
     ...configs,
     visits
   });
+});
+
+router.get('/statistics', async (req, res) => {
+  const addedTodos = await redis.getAsync("addedTodos") || 2;
+
+  res.send({ added_todos: addedTodos });
 });
 
 module.exports = router;
